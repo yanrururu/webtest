f5900841160f3f942ab30fee3a4f38593a5b6beb
yanrururu <yanru0215@gmail.com>
2023-09-19 17:01:32 +0800

    travel_all

--- js/main.js
+++ js/main.js
@@ -1,41 +1,103 @@
-// 漢堡選單
-$(function(){
-  $("#menu").on("click",function(){
-    if($("header").hasClass("openMenu")){
-      $("header").removeClass("openMenu");
-    }
-    else{
-      $("header").addClass("openMenu");
-    }
-  })
-})
 
+// carousel
+var instance = $(".hs__wrapper");
+$.each(instance, function (key, value) {
+  var arrows = $(instance[key]).find(".arrow"),
+    prevArrow = arrows.filter(".arrow-prev"),
+    nextArrow = arrows.filter(".arrow-next"),
+    box = $(instance[key]).find(".hs"),
+    x = 0,
+    mx = 0,
+    maxScrollWidth =
+      box[0].scrollWidth - box[0].clientWidth / 2 - box.width() / 2;
 
-// notice
-var coll = document.getElementsByClassName("notice_btn");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+  $(arrows).on("click", function () {
+    if ($(this).hasClass("arrow-next")) {
+      x = box.width() / 2 + box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: x
+      });
     } else {
-      content.style.display = "block";
+      x = box.width() / 2 - box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: -x
+      });
     }
   });
-}
+
+  $(box).on({
+    mousemove: function (e) {
+      var mx2 = e.pageX - this.offsetLeft;
+      if (mx) this.scrollLeft = this.sx + mx - mx2;
+    },
+    mousedown: function (e) {
+      this.sx = this.scrollLeft;
+      mx = e.pageX - this.offsetLeft;
+    },
+    scroll: function () {
+      toggleArrows();
+    }
+  });
+
+  $(document).on("mouseup", function () {
+    mx = 0;
+  });
+
+  function toggleArrows() {
+    if (box.scrollLeft() > maxScrollWidth - 10) {
+      // disable next button when right end has reached
+      nextArrow.addClass("disabled");
+    } else if (box.scrollLeft() < 10) {
+      // disable prev button when left end has reached
+      prevArrow.addClass("disabled");
+    } else {
+      // both are enabled
+      nextArrow.removeClass("disabled");
+      prevArrow.removeClass("disabled");
+    }
+  }
+});
 
 
-// arrow
-$(function(){
-  $(".notice_btn").on("click",function(){
-    if($(this).find("img").hasClass("rotate")){
-      $(this).find("img").removeClass("rotate");
+
+$(function() {
+    $(".slick").slick({
+
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3.3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [{
+    breakpoint: 1024,
+    settings: {
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    infinite: true,
+    dots: false,
+
     }
-    else{
-      $(this).find("img").addClass("rotate");
+    },
+    {
+    breakpoint: 600,
+    settings: {
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: false,
     }
-  })
-})
+    },
+    {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+    });
+
+    })
